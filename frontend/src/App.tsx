00cd1eafb9d276e84fbe151897a9fe07260b129d
Andrea Panzeri mac <andpanz02@gmail.com>
2026-01-27 22:25:35 +0100

    feat: inizio layout principale

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+import MainLayout from "./component/layout/MainLayout";
 
 
 function App() {
-  
+
+  const [currentPage, setCurrentPage] = useState<string>('dashboard');
+
+
+
+  switch (currentPage) {
+    case 'dashboard':
+      <p>Dashboard</p>
+      break;
+    default:
+      <p>Page not found</p>
+
+  }
+
 
   return (
-    <p>da iniziare</p>
+    <MainLayout currentPage={currentPage} action={setCurrentPage}>
+    </MainLayout>
   );
 }
 
