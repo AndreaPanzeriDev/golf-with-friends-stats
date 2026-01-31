e4aea2ff3bea8f0f90ebf9e81fefde790e17aeb6
Andrea Panzeri mac <andpanz02@gmail.com>
2026-01-31 23:03:57 +0100

    feature: add selected button graphic

--- frontend/src/component/layout/Sidebar.tsx
+++ frontend/src/component/layout/Sidebar.tsx
@@ -13,12 +13,13 @@ function Sidebar({ currentPage, action: setCurrentPage }: PageProps) {
     /**
      * start style
      */
-    const baseButtonStyle = "flex! items-center! justify-start! h-12 w-56 border-none! shadow-none! text-gray-500 text-lg p-5! rounded-xl transition-all hover:bg-[#E6EDE9]! hover:text-[#2C4A39]!"
+    const baseButtonStyle = "flex! items-center! justify-start! h-12 w-56 border-none! shadow-none! text-gray-500 text-lg p-5! rounded-xl transition-all hover:bg-[#E6EDE9]! hover:text-[#2C4A39]! my-1.5!"
+    const activeButton = "bg-[#20553b]! text-white!"
     /**
      * end style
      */
     return (
-        <div className="h-screen w-68 border-r border-gray-200">
+        <div className="h-screen flex! flex-col! w-68 border-r border-gray-200">
             <div className="flex items-center justify-center py-3">
                 <div className="bg-[#2c6141] rounded-full h-11 w-11 justify-center flex items-center mr-3"><span className="text-2xl">⛳</span></div>
                 <div>
@@ -33,15 +34,15 @@ function Sidebar({ currentPage, action: setCurrentPage }: PageProps) {
 
             </div>
             <hr className="border-t border-gray-200" />
-            <div className="flex! flex-1! flex-col! justify-between! items-center! py-6!">
+            <div className="flex! flex-col! items-center justify-between! flex-auto!">
                 <div className="flex! flex-col!">
-                    <Button className={`${baseButtonStyle}`}><LayoutDashboard />Dashboard</Button>
-                    <Button className={`${baseButtonStyle}`}><UserRound />Friends</Button>
-                    <Button className={`${baseButtonStyle}`}><MapPinned />Course</Button>
-                    <Button className={`${baseButtonStyle}`}><LayoutDashboard />Games</Button>
-                    <Button className={`${baseButtonStyle}`}><Calendar />Calendar</Button>
+                    <Button onClick={() => setCurrentPage("dashboard")} className={`${baseButtonStyle} ${currentPage == "dashboard" ? activeButton : ""} `}><LayoutDashboard />Dashboard</Button>
+                    <Button onClick={() => setCurrentPage("friends")} className={`${baseButtonStyle} ${currentPage == "friends" ? activeButton : ""}`}><UserRound />Friends</Button>
+                    <Button onClick={() => setCurrentPage("courses")} className={`${baseButtonStyle} ${currentPage == "courses" ? activeButton : ""}`}><MapPinned />Courses</Button>
+                    <Button onClick={() => setCurrentPage("games")} className={`${baseButtonStyle} ${currentPage == "games" ? activeButton : ""}`}><LayoutDashboard />Games</Button>
+                    <Button onClick={()=>setCurrentPage("dashboard")} className={`${baseButtonStyle} ${currentPage == "calendar" ? activeButton : ""}`}><Calendar />Calendar</Button>
                 </div>
-                <Button className={`${baseButtonStyle}`}><Bolt />Settings</Button>
+                <Button onClick={()=>setCurrentPage("settings")} className={`${baseButtonStyle} ${currentPage == "settings" ? activeButton : ""}` }><Bolt />Settings</Button>
             </div>
         </div>)
 }
