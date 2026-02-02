13c93e04a6195cfb4d45a6f9e1374f54d54cf417
Andrea Panzeri mac <andpanz02@gmail.com>
2026-02-02 20:01:06 +0100

    fix: hover logic and style

--- frontend/src/component/layout/Sidebar.tsx
+++ frontend/src/component/layout/Sidebar.tsx
@@ -13,8 +13,9 @@ function Sidebar({ currentPage, action: setCurrentPage }: PageProps) {
     /**
      * start style
      */
-    const baseButtonStyle = "flex! items-center! justify-start! h-12 w-56 border-none! shadow-none! text-gray-500 text-lg p-5! rounded-xl transition-all hover:bg-[#E6EDE9]! hover:text-[#2C4A39]! my-1.5!"
+    const baseButtonStyle = "flex! items-center! justify-start! h-12 w-56 border-none! shadow-none! text-gray-500 text-lg p-5! rounded-xl transition-all my-1.5!"
     const activeButton = "bg-[#20553b]! text-white!"
+    const hoverButton = "hover:bg-[#E6EDE9]! hover:text-[#2C4A39]!"
     /**
      * end style
      */
@@ -36,13 +37,13 @@ function Sidebar({ currentPage, action: setCurrentPage }: PageProps) {
             <hr className="border-t border-gray-200" />
             <div className="flex! flex-col! items-center justify-between! flex-auto!">
                 <div className="flex! flex-col!">
-                    <Button onClick={() => setCurrentPage("dashboard")} className={`${baseButtonStyle} ${currentPage == "dashboard" ? activeButton : ""} `}><LayoutDashboard />Dashboard</Button>
-                    <Button onClick={() => setCurrentPage("friends")} className={`${baseButtonStyle} ${currentPage == "friends" ? activeButton : ""}`}><UserRound />Friends</Button>
-                    <Button onClick={() => setCurrentPage("courses")} className={`${baseButtonStyle} ${currentPage == "courses" ? activeButton : ""}`}><MapPinned />Courses</Button>
-                    <Button onClick={() => setCurrentPage("games")} className={`${baseButtonStyle} ${currentPage == "games" ? activeButton : ""}`}><LayoutDashboard />Games</Button>
-                    <Button onClick={()=>setCurrentPage("dashboard")} className={`${baseButtonStyle} ${currentPage == "calendar" ? activeButton : ""}`}><Calendar />Calendar</Button>
+                    <Button onClick={() => setCurrentPage("dashboard")} className={`${baseButtonStyle} ${currentPage == "dashboard" ? activeButton : hoverButton} `}><LayoutDashboard />{t("dashboard")}</Button>
+                    <Button onClick={() => setCurrentPage("friends")} className={`${baseButtonStyle} ${currentPage == "friends" ? activeButton :hoverButton}`}><UserRound />{t("friends")}</Button>
+                    <Button onClick={() => setCurrentPage("courses")} className={`${baseButtonStyle} ${currentPage == "courses" ? activeButton :hoverButton}`}><MapPinned />{t("courses")}</Button>
+                    <Button onClick={() => setCurrentPage("games")} className={`${baseButtonStyle} ${currentPage == "games" ? activeButton :hoverButton}`}><LayoutDashboard />{t("games")}</Button>
+                    <Button onClick={()=>setCurrentPage("calendar")} className={`${baseButtonStyle} ${currentPage == "calendar" ? activeButton :hoverButton}`}><Calendar />{t("calendar")}</Button>
                 </div>
-                <Button onClick={()=>setCurrentPage("settings")} className={`${baseButtonStyle} ${currentPage == "settings" ? activeButton : ""}` }><Bolt />Settings</Button>
+                <Button onClick={()=>setCurrentPage("settings")} className={`${baseButtonStyle} ${currentPage == "settings" ? activeButton :hoverButton}` }><Bolt />{t("settings")}</Button>
             </div>
         </div>)
 }
