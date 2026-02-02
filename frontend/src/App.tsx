9af7ef9cbe9b048e71481d7d9c4122741cc005d1
Andrea Panzeri mac <andpanz02@gmail.com>
2026-02-02 21:27:04 +0100

    fix: remove switch logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,19 @@
-import { useState } from "react";
 import MainLayout from "./component/layout/MainLayout";
 
 
 function App() {
 
-  const [currentPage, setCurrentPage] = useState<string>('dashboard');
+  
 
 
 
-  switch (currentPage) {
-    case 'dashboard':
-      <p>Dashboard</p>
-      break;
-    default:
-      <p>Page not found</p>
+  
 
-  }
+  
 
 
   return (
-    <MainLayout currentPage={currentPage} action={setCurrentPage}/>
+    <MainLayout />
   )
 }
 
